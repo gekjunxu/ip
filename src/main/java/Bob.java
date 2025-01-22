24315c0cbd8a2bb3d35c8d08ff05623a3c03f761
gekjunxu <E1122727@u.nus.edu>
2025-01-22 14:40:39 +0800

    Added add and list feature

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.*;
 
@@ -8,13 +9,36 @@ public class Bob {
         String bye = line + "\tBye. Hope to see you again soon!\n" + line;
         System.out.println(hello);
 
+        // Initialise scanner
         Scanner sc = new Scanner(System.in);
+
+        // Read in user input for first time
         String input = sc.nextLine();
+
+        // Store list of to do
+        ArrayList<String> list = new ArrayList<String>();
+
         while (!input.equalsIgnoreCase("bye")) {
-            String output = String.format(line + "\t" + input + "\n" +  line);
-            System.out.println(output);
+            if (input.equalsIgnoreCase("list")) {
+                // List out the list
+               if (list.isEmpty()) {
+                   System.out.println("There are no items in the list");
+               } else {
+                   for (int i = 0; i < list.size(); i++) {
+                       System.out.println("\t" + (i+1) + ". " + list.get(i));
+                   }
+               }
+
+            } else {
+                // Add item to list
+                list.add(input);
+                System.out.println(line + "\tadded: " + input + "\n" + line);
+            }
+
+            // Continue to read next user input
             input = sc.nextLine();
         }
+        // Close the bot when user types bye
         System.out.println(bye);
 
     }

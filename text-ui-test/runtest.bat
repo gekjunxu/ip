09ef5a3314e0fa6600ac3aa8a5d9810c739a118e
gekjunxu <e1122727@u.nus.edu>
2025-01-31 14:31:29 +0800

    Update test script

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -9,7 +9,8 @@ if exist ACTUAL.TXT del ACTUAL.TXT
 if exist data\bob.txt del data\bob.txt
 
 REM compile the code into the bin folder
-javac  -cp ..\src\main\java\bob -Xlint:none -d ..\bin ..\src\main\java\bob\*.java
+javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\bob\*.java
+
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
